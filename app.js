c41acb4bcfa2753573650ce8722e0e17e643586a
Eduardo Yoshio <eduardo.yoshio@outlook.com>
2022-08-13 21:10:48 -0300

    adding more routes and creating services

--- app.js
+++ app.js
@@ -1,69 +1,105 @@
 const express=require('express');
-const mysql=require('mysql');
+// const mysql=require('mysql');
 const bodyParser=require('body-parser');
 const app=express();
+
+const CommentService=require('./services/CommentService');
+const PostService=require('./services/PostService');
+
+let commentObj = new CommentService();
+let postObj = new PostService();
 
 app.use(bodyParser.urlencoded({
     extended: true
 }));
 app.use(bodyParser.json());
 
-const sql=mysql.createPool({
-    host:'localhost',
-    user:'root',
-    password:'',
-    port:3306
- });
- sql.query("use nodejsapi");
+// const sql=mysql.createPool({
+//     host:'localhost',
+//     user:'root',
+//     password:'',
+//     port:3306
+//  });
+//  sql.query("use nodejsapi");
 
  var baseUrl = "/api";
-//Routes
-app.get(baseUrl, function(req,res){
-    res.send("Início da Aplicação");
+
+//Post Routes
+app.get(baseUrl, (req,res) => {
+    res.send("Aplication start");
 });
 
 app.get(baseUrl + "/posts", (req, res) => {
-    // GetUser(req,res)
-    // {
-        // if(!req.params.id){
-            sql.getConnection((err,connection) => {
-                connection.query("select * from posts order by id asc", function(err,results,fields){
-                    if (err) throw err;
-                    res.send({results});
-                    connection.release();
-                });
-            });
-        // }
-        // else{
-        //     this.sql.getConnection(function(err,connection){
-        //         sql.query("select * from user where id=? order by id asc",[req.params.id],function(err,results,fields){
-        //             res.render('select',{data:results});
-        //         });
-        //     });
-        // }
-    // }
+    if(req.query.with_text != undefined) {
+        // res.status(200).send(req.query.with_text);
+        postObj.GetPostListWithText(req, res);
+    }
+    else{
+        postObj.GetPostList(req,res);
+    }
+});
 
+// app.get(baseUrl + "/posts/:id?", (req, res) => {
+//     postObj.GetPost(req, res);
+// });
+
+app.get(baseUrl + "/posts/:id", (req, res) => {
+    if(req.query.with_comments != undefined) {
+        postObj.GetPostWithComments(req, res);
+    }
+    else{
+        postObj.GetPost(req, res);
+    }
 });
 
 app.post(baseUrl + "/posts", (req, res) => {
-        sql.getConnection(function(err,connection){
-            connection.query("insert into posts (userId, title, body) values (?, ?, ?)",[req.body.userId,req.body.title,req.body.body]);
-            if (err) throw err;
-            connection.release();
-    });
-    res.send(req.body);
-    console.log("dados inseridos!");
-  });
+    postObj.AddPost(req, res);''
+});
+
+ 
+app.put(baseUrl + "/posts/:id", (req, res) => {
+    postObj.UpdatePost(req, res);
+});
+
+app.patch(baseUrl + "/posts/:id", (req, res) => {
+    postObj.UpdatePostInfo(req, res);
+});
+
+app.delete(baseUrl + "/posts/:id", (req, res) => {
+    postObj.DeletePost(req, res);
+});
 
 
 
+//Comment Routes
+app.get(baseUrl + "/comments", (req, res) => {
+    commentObj.GetCommentList(req, res);
+});
 
+app.get(baseUrl + "/comments/:id?", (req, res) => {
+    commentObj.GetComment(req, res);
+});
 
+app.post(baseUrl + "/comments", (req, res) => {
+    commentObj.AddComment(req, res);
+});
+
+app.patch(baseUrl + "/comments/:id", (req, res) => {
+    commentObj.UpdateCommentInfo(req, res);
+});
+
+app.delete(baseUrl + "/comments/:id", (req, res) => {
+    commentObj.DeleteComment(req, res);
+});
+
+app.put(baseUrl + "/comments/:id", (req, res) => {
+    commentObj.UpdateComment(req, res);
+});
 
  //Database
 let port=process.env.PORT || 3000;
 
 //Start server
 app.listen(port,function(req,res){
-    console.log('Servidor está rodando!');
+    console.log('Server is running!');
  });
